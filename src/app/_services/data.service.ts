4cf570686981247e5d362d4083bc51cce1af92cb
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-10 22:56:57 +0530

    done

--- src/app/_services/data.service.ts
+++ src/app/_services/data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Todo } from '../_models/todo.model';
 import { Observable } from 'rxjs';
 
@@ -15,5 +15,16 @@ export class DataService {
     return this.http.get<Todo[]>(
       'https://jsonplaceholder.typicode.com/todos'
     ) as Observable<Todo[]>;
+  }
+
+  createToDoItem(item: Todo) {
+    const customHeaders = new HttpHeaders()
+      .set('X-Custom-Header-A', 'Header-A-Value')
+      .set('X-Custom-Header-B', 'Header-B-Value')
+      .append('My-Header', 'My-Value');
+
+    return this.http.post('http://localhost:3000/api/items', item, {
+      headers: customHeaders
+    });
   }
 }
--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataService } from '../_services/data.service';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Todo } from '../_models/todo.model';
 
 @Component({
@@ -8,12 +8,32 @@ import { Todo } from '../_models/todo.model';
   templateUrl: './playground.component.html',
   styleUrls: ['./playground.component.css']
 })
-export class PlaygroundComponent implements OnInit {
+export class PlaygroundComponent implements OnInit, OnDestroy {
   items$: Observable<Todo[]>;
+  httpSubscription: Subscription;
 
   constructor(private dataService: DataService) {}
 
   ngOnInit() {
     this.items$ = this.dataService.getToDoItems();
+  }
+
+  handler() {
+    this.httpSubscription = this.dataService
+      .createToDoItem({
+        id: 100,
+        completed: false,
+        title: 'Title',
+        userId: 5012
+      })
+      .subscribe(
+        data => console.log('Http Post Success', data),
+        error => console.error('Http Post Failure', error),
+        () => console.log('Http Complete')
+      );
+  }
+
+  ngOnDestroy(): void {
+    this.httpSubscription.unsubscribe();
   }
 }
